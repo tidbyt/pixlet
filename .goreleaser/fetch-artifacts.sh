aa51f8f3e640ea431a55a14b284b7d0cc3960614
Rohan Singh <rohan@washington.edu>
2020-10-19 03:52:00 +0000

    goreleaser: Fix artifact download paths

--- .goreleaser/fetch-artifacts.sh
+++ .goreleaser/fetch-artifacts.sh
@@ -7,5 +7,6 @@ buildkite-agent artifact download '*' ./artifacts
 
 for dist in "darwin_amd64" "linux_amd64"; do
     mkdir -p "dist/$dist"
-    cp "artifacts/$dist" "dist/$dist/pixlet"
+    cp "artifacts/$dist" "dist/pixlet_$dist/pixlet"
+    chmod +x "dist/pixlet_$dist/pixlet"
 done
