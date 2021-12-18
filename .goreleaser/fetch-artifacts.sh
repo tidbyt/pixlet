178d4596767a8388630953ff05465b8d2011f042
Sam Moore <sam@betterment.com>
2021-12-18 17:18:23 -0500

    feat(arm64-support): add arm64 to goreleaser config
    this adds arm64 to the list of supported go architectures in the
    goreleaser configuration.
    
    it also updates the fetch-artifacts.sh script to copy the linux and
    darwin arm64 artifacts. i'm not entirely sure how this fits into the
    buildkite CI process, but it seems like it might need to be updated.
    
    i'm able to run the goreleaser command locally and see it generate all
    the proper files.
    
    one interesting and important note about building the render module
    while running on macos, i did need to set up the CPATH and LIBRARY_PATH
    as described here https://github.com/tidbyt/pixlet/issues/39#issuecomment-900727783

--- .goreleaser/fetch-artifacts.sh
+++ .goreleaser/fetch-artifacts.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 mkdir -p artifacts
 buildkite-agent artifact download '*' ./artifacts
 
-for dist in "darwin_amd64" "linux_amd64"; do
+for dist in "darwin_amd64" "linux_amd64" "darwin_arm64" "linux_arm64"; do
     mkdir -p "dist/$dist"
     cp "artifacts/$dist" "dist/pixlet_$dist/pixlet"
     chmod +x "dist/pixlet_$dist/pixlet"
