e36fa26f673fee201d5748e1ae851da241d292e1
Mark Spicer <spicer93@gmail.com>
2021-12-29 22:41:30 +0000

    Updated to staticly link Linux binaries.
    This commit updates both Linux binaries to use the ones available
    through offical Ubuntu installs even though they are old. In addition,
    it ensures we are statically linking the webp library for Linux builds.

--- scripts/fetch-deps.sh
+++ scripts/fetch-deps.sh
@@ -17,9 +17,6 @@ pushd "/tmp/$LIBWEBP_VERSION" > /dev/null
 # WebP release signing key.
 gpg --receive-keys --keyserver hkps://keyserver.ubuntu.com 6B0E6B70976DE303EDF2F601F9C3D6BDB8232B5D 2>/dev/null
 
-# Arch Linux ARM Build System.
-gpg --receive-keys --keyserver hkps://keyserver.ubuntu.com 68B3537F39A313B3E574D06777193F152BDBE6A6 2>/dev/null
-
 echo "Fetching WebP Binaries"
 for ARCH in $RELEASE_ARCHS
 do
@@ -28,16 +25,6 @@ do
 		curl -sLO "https://storage.googleapis.com/downloads.webmproject.org/releases/webp/${LIBWEBP_VERSION}-${ARCH}.zip.asc"
 		gpg --verify "${LIBWEBP_VERSION}-${ARCH}.zip.asc" "${LIBWEBP_VERSION}-${ARCH}.zip" 2>/dev/null
 		unzip -q "${LIBWEBP_VERSION}-${ARCH}.zip" -d "${ARCH}"
-	elif [[ $ARCH == "linux-arm64"  ]]; then
-		# TODO: there is no official release for aarch64, so we need to find it elsewhere. Unfortunately, we need
-		# the RC version to get macOS M1 support. The RC version is not available under this mirror so we are
-		# using a different version. It feels bad, but it allows us to build for all target platforms in the short
-		# term.
-		curl -sLO "http://mirror.archlinuxarm.org/aarch64/extra/libwebp-1.2.1-2-aarch64.pkg.tar.xz"
-		curl -sLO "http://mirror.archlinuxarm.org/aarch64/extra/libwebp-1.2.1-2-aarch64.pkg.tar.xz.sig"
-		gpg --verify "libwebp-1.2.1-2-aarch64.pkg.tar.xz.sig" "libwebp-1.2.1-2-aarch64.pkg.tar.xz" 2>/dev/null
-		mkdir "linux-arm64"
-		tar -xf libwebp-1.2.1-2-aarch64.pkg.tar.xz -C "linux-arm64"
 	else
 		curl -sLO "https://storage.googleapis.com/downloads.webmproject.org/releases/webp/${LIBWEBP_VERSION}-${ARCH}.tar.gz"
 		curl -sLO "https://storage.googleapis.com/downloads.webmproject.org/releases/webp/${LIBWEBP_VERSION}-${ARCH}.tar.gz.asc"
