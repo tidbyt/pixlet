48d115cdf85064a03782d7ec58b33cf31a901fdb
Mark Spicer <spicer93@gmail.com>
2022-01-03 16:21:41 -0500

    feat(release): Move release to GitHub Actions. (#104)
    * feat(release): Move release to GitHub Actions.
    
    This commit moves our release process to GitHub actions. The way this
    used to work was through our internal build system which makes it really
    hard to give any visibility to anyone else. In addition, we were hitting
    the limits of our local macOS build system and needed an upgraded agent.
    
    * Split workflows.
    
    This commit splits workflows so that pull requests and main have
    differnt workflows.
    
    * Update .github/workflows/main.yml
    
    Co-authored-by: Rohan Singh <rohan@tidbyt.com>
    
    Co-authored-by: Rohan Singh <rohan@tidbyt.com>

--- .goreleaser/fetch-artifacts.sh
+++ .goreleaser/fetch-artifacts.sh
@@ -2,11 +2,8 @@
 
 set -euo pipefail
 
-mkdir -p artifacts
-buildkite-agent artifact download '*' ./artifacts
-
 for dist in "darwin_amd64" "linux_amd64" "darwin_arm64" "linux_arm64"; do
-    mkdir -p "dist/$dist"
-    cp "artifacts/$dist" "dist/pixlet_$dist/pixlet"
+    mkdir -p "dist/pixlet_$dist"
+    cp "$dist/pixlet" "dist/pixlet_$dist/pixlet"
     chmod +x "dist/pixlet_$dist/pixlet"
 done
