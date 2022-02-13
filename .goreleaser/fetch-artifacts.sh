225b0f4a85f4dccf9cca258edaf55cd9d85bd434
Mark Spicer <mark@tidbyt.com>
2022-02-13 21:23:12 +0000

    devtools: Move goreleaser to use ./out.
    This commit updates goreleaser to not clash with the dist directory that
    is not being used with the React frontend.

--- .goreleaser/fetch-artifacts.sh
+++ .goreleaser/fetch-artifacts.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 for dist in "darwin_amd64" "linux_amd64" "darwin_arm64" "linux_arm64"; do
-    mkdir -p "dist/pixlet_$dist"
-    cp "$dist/pixlet" "dist/pixlet_$dist/pixlet"
-    chmod +x "dist/pixlet_$dist/pixlet"
+    mkdir -p "out/pixlet_$dist"
+    cp "$dist/pixlet" "out/pixlet_$dist/pixlet"
+    chmod +x "out/pixlet_$dist/pixlet"
 done
