1f41534332d1eb1253ba2159d67f295f66bba5ed
Felix Bruns <felixbruns@spotify.com>
2022-03-07 20:34:49 +0100

    animation: Add `Curve` parsing and reversing.
    * Add `Reverse` function on `Curve` interface.
    * Implement `Reverse` for linear and cubic bezier curve.
    * Implement `ParseCurve`, which can handle predefined and custom cubic bezier curves.

--- render/animation/curve.go
+++ render/animation/curve.go
@@ -1,6 +1,11 @@
 package animation
 
-import "math"
+import (
+	"fmt"
+	"math"
+	"regexp"
+	"strconv"
+)
 
 var EaseIn = CubicBezierCurve{0.3, 0, 1, 1}
 var EaseOut = CubicBezierCurve{0, 0, 0, 1}
@@ -13,6 +18,7 @@ var EaseInOut = CubicBezierCurve{0.65, 0, 0.35, 1}
 
 type Curve interface {
 	Transform(t float64) float64
+	Reverse() Curve
 }
 
 // Linear curve moving from 0 to 1 (wait for it...) linearly
@@ -20,6 +26,10 @@ type LinearCurve struct{}
 
 func (lc LinearCurve) Transform(t float64) float64 {
 	return t
+}
+
+func (lc LinearCurve) Reverse() Curve {
+	return LinearCurve{}
 }
 
 // Bezier curve defined by a, b, c and d.
@@ -47,6 +57,51 @@ func (cb CubicBezierCurve) Transform(t float64) float64 {
 	return math.NaN()
 }
 
+func (cb CubicBezierCurve) Reverse() Curve {
+	return CubicBezierCurve{1 - cb.c, 1 - cb.d, 1 - cb.a, 1 - cb.b}
+}
+
 func (cb CubicBezierCurve) computeBezier(t, e, f float64) float64 {
 	return 3*e*(1-t)*(1-t)*t + 3*f*(1-t)*t*t + t*t*t
+}
+
+var cubicBezierRe = regexp.MustCompile(
+	`^cubic-bezier\(` +
+		`(?P<a>[+-]?([0-9]*\.)?[0-9]+), ` +
+		`(?P<b>[+-]?([0-9]*\.)?[0-9]+), ` +
+		`(?P<c>[+-]?([0-9]*\.)?[0-9]+), ` +
+		`(?P<d>[+-]?([0-9]*\.)?[0-9]+)` +
+		`\)$`)
+
+func ParseCurve(scurve string) (Curve, error) {
+	match := cubicBezierRe.FindStringSubmatch(scurve)
+	if match != nil {
+		result := make(map[string]string)
+
+		for i, name := range cubicBezierRe.SubexpNames() {
+			if i != 0 && name != "" {
+				result[name] = match[i]
+			}
+		}
+
+		a, _ := strconv.ParseFloat(result["a"], 64)
+		b, _ := strconv.ParseFloat(result["b"], 64)
+		c, _ := strconv.ParseFloat(result["c"], 64)
+		d, _ := strconv.ParseFloat(result["d"], 64)
+
+		return CubicBezierCurve{a, b, c, d}, nil
+	}
+
+	switch scurve {
+	case "linear":
+		return LinearCurve{}, nil
+	case "ease_in":
+		return EaseIn, nil
+	case "ease_out":
+		return EaseOut, nil
+	case "ease_in_out":
+		return EaseInOut, nil
+	default:
+		return LinearCurve{}, fmt.Errorf("curve: %v is not a valid curve name", scurve)
+	}
 }
