8d0eca8f95e451de607d4cc64f3fe158ef450028
Felix Bruns <felixbruns@spotify.com>
2022-03-26 17:40:50 +0100

    render/animation: Support custom easing curves.

--- runtime/modules/animation_runtime/curve.go
+++ runtime/modules/animation_runtime/curve.go
@@ -9,9 +9,23 @@ import (
 )
 
 func CurveFromStarlark(value starlark.Value) (animation.Curve, error) {
-	if str, ok := starlark.AsString(value); ok {
-		return animation.ParseCurve(str)
+	if str, ok := value.(starlark.String); ok {
+		if str.Len() == 0 {
+			return animation.LinearCurve{}, nil
+		} else if curve, err := animation.ParseCurve(str.GoString()); err == nil {
+			return curve, nil
+		} else {
+			return animation.LinearCurve{}, fmt.Errorf("curve is not a valid curve string: %s", str.GoString())
+		}
 	}
 
-	return nil, fmt.Errorf("invalid type for curve: %s (expected string)", value.Type())
+	if fn, ok := value.(*starlark.Function); ok {
+		if fn.NumParams() != 1 || fn.NumKwonlyParams() != 0 {
+			return animation.LinearCurve{}, fmt.Errorf("invalid number of parameters to curve function: %s", fn.String())
+		}
+
+		return animation.CustomCurve{Function: fn}, nil
+	}
+
+	return animation.LinearCurve{}, nil
 }
