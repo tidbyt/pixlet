87fe8da65ab3bfb8eea9bedb46be3a462f1bfd03
Mark Spicer <mark@tidbyt.com>
2022-04-14 11:02:21 -0400

    bugfix: Fix dist dir build issues (#252)
    * dist: Create basic go structure.
    
    This commit creates the basic go structure to be able to build without
    the frontend build.
    
    * dist: Cleanup frontend build.
    
    This commit makes it so that a build + clean resets the repo back to the
    expected state.

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,18 +2,11 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const path = require('path');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
 
 const htmlPlugin = new HtmlWebPackPlugin({
     template: './src/index.html',
     filename: '../index.html',
     favicon: 'src/favicon.png'
-});
-
-const copyPlugin = new CopyPlugin({
-    patterns: [
-        { from: "src/dist.go.txt", to: "../dist.go" },
-    ],
 });
 
 module.exports = merge(common, {
@@ -26,5 +19,5 @@ module.exports = merge(common, {
         filename: '[name].[chunkhash].js',
         clean: true,
     },
-    plugins: [htmlPlugin, copyPlugin]
+    plugins: [htmlPlugin]
 });
