dbcd2a4ba4e2e2a685439a5cf47a03a9673b43f7
Mark Spicer <mark@tidbyt.com>
2022-10-28 21:51:46 +0000

    Fix script

--- scripts/release-windows.sh
+++ scripts/release-windows.sh
@@ -5,6 +5,4 @@ set -e
 export RELEASE_ARCHS="windows-x86_64"
 export RELEASE_PLATFORM="windows"
 
-source scripts/set-libwebp-version.sh
-source scripts/fetch-deps.sh
 source scripts/build-release.sh
