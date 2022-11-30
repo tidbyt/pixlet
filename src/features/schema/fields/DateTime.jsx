9f476ac3c87dace8643d4cd9721bbf0428358644
Mark Spicer <mark@tidbyt.com>
2022-11-30 09:47:11 -0500

    bugfix: Fix DateTime picker. (#501)
    We origianlly used a lab feature of material UI that has been moved to
    it's own package. Unfortuantely, it simply failed with a warning in the
    console instead of failing to build. This commit updates to use the new
    package.

--- src/features/schema/fields/DateTime.jsx
+++ src/features/schema/fields/DateTime.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
-import AdapterDateFns from '@mui/lab/AdapterDateFns';
-import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
 import TextField from '@mui/material/TextField';
-import DateTimePicker from '@mui/lab/DateTimePicker';
 
 import { set, remove } from '../../config/configSlice'
 
@@ -35,7 +35,7 @@ export default function DateTime({ field }) {
     }
 
     return (
-        <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
             <DateTimePicker
                 renderInput={(props) => <TextField {...props} />}
                 label={field.name}
