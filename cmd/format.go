0f1aeda1aece3e1381491243cfdde37c593af382
Mark Spicer <mark@tidbyt.com>
2023-01-09 21:18:30 +0000

    Clean up format command and interface.
    This commit cleans up the format command and interface to behave has we
    disscussed in Discord.

--- cmd/format.go
+++ cmd/format.go
@@ -1,10 +1,8 @@
 package cmd
 
 import (
-	"fmt"
 	"os"
 
-	"github.com/bazelbuild/buildtools/buildifier/utils"
 	"github.com/bazelbuild/buildtools/differ"
 	"github.com/spf13/cobra"
 )
@@ -12,41 +10,43 @@ import (
 func init() {
 	FormatCmd.Flags().BoolVarP(&vflag, "verbose", "v", false, "print verbose information to standard error")
 	FormatCmd.Flags().BoolVarP(&rflag, "recursive", "r", false, "find starlark files recursively")
-	FormatCmd.Flags().BoolVarP(&dryRunFlag, "dry-run", "d", false, "no code modifications")
-	FormatCmd.Flags().StringVarP(&format, "format", "f", "", "diagnostics format: text or json (default text)")
+	FormatCmd.Flags().BoolVarP(&dryRunFlag, "dry-run", "d", false, "display a diff of formatting changes without modification")
+	FormatCmd.Flags().StringVar(&format, "format", "text", "diagnostics format: text or json")
 }
 
 var FormatCmd = &cobra.Command{
-	Use:   "format",
-	Short: "Formats Pixlet apps.",
-	Run:   formatCmd,
+	Use:   "format <pathspec>...",
+	Short: "Formats Tidbyt apps",
+	Example: `  pixlet format app.star
+  pixlet format app.star --dry-run
+  pixlet format --recursive ./`,
+	Long: `The format command provides a code formatter for Tidbyt apps. By default, it
+will format your starlark source code in line. If you wish you see the output
+before applying, add the --dry-run flag.`,
+	Args: cobra.MinimumNArgs(1),
+	Run:  formatCmd,
 }
 
 func formatCmd(cmd *cobra.Command, args []string) {
-	mode := "fix"
+	// Lint refers to the lint mode for buildifier, with the options being off,
+	// warn, or fix. For pixlet format, we don't want to lint at all.
 	lint := "off"
 
+	// Mode refers to formatting mode for buildifier, with the options being
+	// check, diff, or fix. For the pixlet format command, we want to fix the
+	// resolvable issue by default and provide a dry run flag to be able to
+	// diff the changes before fixing them.
+	mode := "fix"
 	if dryRunFlag {
 		mode = "diff"
 	}
 
-	if err := utils.ValidateFormat(&format, &mode); err != nil {
-		fmt.Fprintf(os.Stderr, "buildifier: %s\n", err)
-		os.Exit(2)
-	}
-
-	dflag := false
-	if err := utils.ValidateModes(&mode, &lint, &dflag); err != nil {
-		fmt.Fprintf(os.Stderr, "buildifier: %s\n", err)
-		os.Exit(2)
-	}
-
-	differ, deprecationWarning := differ.Find()
-	if deprecationWarning && mode == "diff" {
-		fmt.Fprintf(os.Stderr, "buildifier: selecting diff program with the BUILDIFIER_DIFF, BUILDIFIER_MULTIDIFF, and DISPLAY environment variables is deprecated, use flags -diff_command and -multi_diff instead\n")
-	}
+	// Copied from the buildifier source, we need to supply a diff program for
+	// the differ.
+	differ, _ := differ.Find()
 	diff = differ
 
+	// Run buildifier and exit with the returned exit code.
 	exitCode := runBuildifier(args, lint, mode, format, rflag, vflag)
 	os.Exit(exitCode)
 }
