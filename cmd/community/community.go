abdfbd1cd8bb1b4f1141bf458e9390e79e6136ff
Mark Spicer <mark@tidbyt.com>
2023-01-18 20:58:26 +0000

    community: Add validate-manifest command.
    This commit adds a command to the community sub-command that will take a
    manifest file as an argument and validate that it's correct.

--- cmd/community/community.go
+++ cmd/community/community.go
@@ -6,6 +6,7 @@ import (
 
 func init() {
 	CommunityCmd.AddCommand(TargetDeterminatorCmd)
+	CommunityCmd.AddCommand(ValidateManifestCmd)
 }
 
 var CommunityCmd = &cobra.Command{
---
+++ cmd/community/validatemanifest.go
@@ -0,0 +1,44 @@
+package community
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/spf13/cobra"
+	"tidbyt.dev/pixlet/manifest"
+)
+
+var ValidateManifestCmd = &cobra.Command{
+	Use:     "validate-manifest <pathspec>",
+	Short:   "Validates an app manifest is ready for publishing",
+	Example: `  pixlet community validate-manifest manifest.yaml`,
+	Long: `This command determines if your app manifest is configured properly by
+validating the contents of each field.`,
+	Args: cobra.ExactArgs(1),
+	RunE: validateManifest,
+}
+
+func validateManifest(cmd *cobra.Command, args []string) error {
+	fileName := filepath.Base(args[0])
+	if fileName != "manifest.yaml" && fileName != "manifest.yml" {
+		return fmt.Errorf("supplied manifest must be named manifest.yaml or manifest.yml")
+	}
+
+	f, err := os.Open(args[0])
+	if err != nil {
+		return fmt.Errorf("couldn't open manifest: %w", err)
+	}
+
+	m, err := manifest.LoadManifest(f)
+	if err != nil {
+		return fmt.Errorf("couldn't load manifest: %w", err)
+	}
+
+	err = m.Validate()
+	if err != nil {
+		return fmt.Errorf("couldn't validate manifest: %w", err)
+	}
+
+	return nil
+}
