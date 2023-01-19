c1c23f79033df3c264b35abf31fe566215a03b7c
dinosaursrarr <105217023+dinosaursrarr@users.noreply.github.com>
2023-01-19 22:26:33 +0000

    Create InputSlider widget
    Latitude and longitude are numbers with a bounded range, so a slider makes sense. But I would often want to test a specific location so I also want to be able to type in the number.
    
    This creates a linked slider and numerical input. Changing one changes the other, and both have the same limits.
    
    Largely based on an example on the MUI site.
    https://mui.com/material-ui/react-slider/#InputSlider.js

---
+++ src/features/schema/fields/location/InputSlider.jsx
@@ -0,0 +1,75 @@
+// https://mui.com/material-ui/react-slider/#InputSlider.js
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Slider from '@mui/material/Slider';
+import MuiInput from '@mui/material/Input';
+
+const Input = styled(MuiInput)`
+  width: 80px;
+`;
+
+export default function InputSlider({ min, max, step }) {
+  const [value, setValue] = React.useState(0);
+
+  const handleSliderChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const handleInputChange = (event) => {
+    if (event.target.value === '') {
+      setValue('');
+      return;
+    }
+    const value = Number(event.target.value);
+    if (value < min) {
+      setValue(min);
+    } else if (value > max) {
+      setValue(max);
+    } else {
+      setValue(value);
+    }
+  };
+
+  const handleBlur = () => {
+    if (value < min) {
+      setValue(min);
+    } else if (value > max) {
+      setValue(max);
+    }
+  };
+
+  return (
+    <Box sx={{ width: 250 }}>
+      <Grid container spacing={2} alignItems="center">
+        <Grid item xs>
+          <Slider
+            value={typeof value === 'number' ? value : 0}
+            min={min}
+            max={max}
+            step={step}
+            onChange={handleSliderChange}
+            aria-labelledby="input-slider"
+          />
+        </Grid>
+        <Grid item>
+          <Input
+            value={value}
+            size="small"
+            onChange={handleInputChange}
+            onBlur={handleBlur}
+            inputProps={{
+              step: {step},
+              min: {min},
+              max: {max},
+              type: 'number',
+              'aria-labelledby': 'input-slider',
+            }}
+          />
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
