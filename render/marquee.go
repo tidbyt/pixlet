8009cf553045713c1528263358120025b1244621
Kevin Chung <kchung@ctfd.io>
2023-01-22 02:28:49 -0500

    Revert doc formatting change

--- render/marquee.go
+++ render/marquee.go
@@ -37,12 +37,10 @@ import (
 //
 // EXAMPLE BEGIN
 // render.Marquee(
-//
-//	width=64,
-//	child=render.Text("this won't fit in 64 pixels"),
-//	offset_start=5,
-//	offset_end=32,
-//
+//      width=64,
+//      child=render.Text("this won't fit in 64 pixels"),
+//      offset_start=5,
+//      offset_end=32,
 // )
 // EXAMPLE END
 type Marquee struct {
