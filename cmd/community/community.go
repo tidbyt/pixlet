1ee26871f24ebb73a56c6fcde4e8d926676ea7c8
Mark Spicer <mark@tidbyt.com>
2023-01-23 17:47:41 +0000

    community: Add command to list icons.
    This commit adds a command to list valid icons. This will be useful for
    the check command so users can find valid icons.

--- cmd/community/community.go
+++ cmd/community/community.go
@@ -6,6 +6,7 @@ import (
 
 func init() {
 	CommunityCmd.AddCommand(CreateManifestCmd)
+	CommunityCmd.AddCommand(ListIconsCmd)
 	CommunityCmd.AddCommand(TargetDeterminatorCmd)
 	CommunityCmd.AddCommand(ValidateIconsCmd)
 	CommunityCmd.AddCommand(ValidateManifestCmd)
