ef33100e905650725fabb8688ae17a22911552e5
Mark Spicer <mark@tidbyt.com>
2023-03-06 18:37:32 +0000

    feat: Add bundle CLI command.
    This commit adds a CLI command to produce a bundle.

--- main.go
+++ main.go
@@ -30,6 +30,7 @@ func init() {
 	rootCmd.AddCommand(cmd.LintCmd)
 	rootCmd.AddCommand(cmd.CreateCmd)
 	rootCmd.AddCommand(cmd.CheckCmd)
+	rootCmd.AddCommand(cmd.BundleCmd)
 	rootCmd.AddCommand(community.CommunityCmd)
 }
 
