359ff08f4e14618d1e1b1c569856945526effd51
Mark Spicer <mark@tidbyt.com>
2023-03-06 18:01:04 +0000

    feat: Add app bundles module.
    This commit adds a module that can create and extract bundles of apps.
    This will be useful for being able to create portable apps in a
    standardized format.

---
+++ bundle/bundle_test.go
@@ -0,0 +1,56 @@
+package bundle_test
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"tidbyt.dev/pixlet/bundle"
+)
+
+func TestBundleWriteAndLoad(t *testing.T) {
+	// Ensure we can load the bundle from an app.
+	ab, err := bundle.InitFromPath("testdata/testapp")
+	assert.NoError(t, err)
+	assert.Equal(t, "test-app", ab.Manifest.ID)
+	assert.Equal(t, 633, len(ab.Source))
+
+	// Create a temp directory.
+	dir, err := os.MkdirTemp("", "")
+	assert.NoError(t, err)
+
+	// Write bundle to the temp directory.
+	err = ab.WriteBundleToPath(dir)
+	assert.NoError(t, err)
+
+	// Ensure we can load up the bundle just created.
+	path := filepath.Join(dir, bundle.AppBundleName)
+	f, err := os.Open(path)
+	assert.NoError(t, err)
+	defer f.Close()
+	newBun, err := bundle.LoadBundle(f)
+	assert.NoError(t, err)
+	assert.Equal(t, "test-app", newBun.Manifest.ID)
+	assert.Equal(t, 633, len(ab.Source))
+}
+func TestLoadBundle(t *testing.T) {
+	f, err := os.Open("testdata/bundle.tar.gz")
+	assert.NoError(t, err)
+	defer f.Close()
+	ab, err := bundle.LoadBundle(f)
+	assert.NoError(t, err)
+	assert.Equal(t, "test-app", ab.Manifest.ID)
+	assert.Equal(t, 633, len(ab.Source))
+}
+
+func TestLoadBundleExcessData(t *testing.T) {
+	f, err := os.Open("testdata/excess-files.tar.gz")
+	assert.NoError(t, err)
+	defer f.Close()
+
+	ab, err := bundle.LoadBundle(f)
+	assert.NoError(t, err)
+	assert.Equal(t, "test-app", ab.Manifest.ID)
+	assert.Equal(t, 633, len(ab.Source))
+}
