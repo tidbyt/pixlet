37df98a49f8cd0f696497e33ece44390937bbb50
Mark Spicer <mark@tidbyt.com>
2023-03-23 00:56:22 +0000

    feat: Add palette support.
    The schema field supports a palette, but the color picker did not. This
    commit adds palette support.

--- src/features/schema/fields/Color.jsx
+++ src/features/schema/fields/Color.jsx
@@ -7,9 +7,12 @@ import { set } from '../../config/configSlice';
 
 export default function Color({ field }) {
     const [color, setColor] = useState(createColor(field.default));
+    const [palette, setPalette] = useState(field.palette);
     const config = useSelector(state => state.config);
     const dispatch = useDispatch();
 
+    // TODO: figure out how to update the palette when schema changes without
+    // a refresh.
     useEffect(() => {
         if (field.id in config) {
             setColor(createColor(config[field.id].value));
@@ -36,6 +39,6 @@ export default function Color({ field }) {
     }
 
     return (
-        <ColorPicker value={color} hideTextfield disablePlainColor onChange={onChange} />
+        <ColorPicker value={color} hideTextfield disablePlainColor palette={palette} onChange={onChange} />
     )
 }
--- src/features/watcher/watcher.js
+++ src/features/watcher/watcher.js
@@ -9,7 +9,7 @@ export default class Watcher {
     }
 
     connect() {
-        const proto =  document.location.protocol === "https:" ? "wss:" : "ws:";
+        const proto = document.location.protocol === "https:" ? "wss:" : "ws:";
         this.conn = new WebSocket(proto + '//' + document.location.host + '/api/v1/ws');
         this.conn.open = this.open.bind(this);
         this.conn.onmessage = this.process.bind(this);
