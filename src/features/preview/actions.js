90d970e3d17c852d898b80a0c51429df84958423
Mark Spicer <mark@tidbyt.com>
2023-05-23 17:13:52 -0400

    wasm: Debounce the previews in WASM. (#791)
    * wasm: Debounce the previews in WASM.
    
    This commit delays the preview requests when using the WASM editor since
    it can't handle the same throughput as a locally running pixlet.
    
    * Remove title for wasm

--- src/features/preview/actions.js
+++ src/features/preview/actions.js
@@ -3,24 +3,49 @@ import { update, loading } from './previewSlice';
 import { set as setError, clear as clearErrors } from '../errors/errorSlice';
 import store from '../../store';
 
+let timeout = null;
 
 export default function fetchPreview(formData) {
-    store.dispatch(loading(true));
-    axios.post(`${PIXLET_API_BASE}/api/v1/preview`, formData)
-        .then(res => {
-            document.title = res.data.title;
-            store.dispatch(update(res.data));
-            if ('error' in res.data) {
-                store.dispatch(setError({ id: res.data.error, message: res.data.error }));
-            } else {
-                store.dispatch(clearErrors());
-            }
-        })
-        .catch(err => {
-            // TODO: fix this.
-            store.dispatch(setError({ id: err, message: err }));
-        })
-        .then(() => {
-            store.dispatch(loading(false));
-        })
+    if (PIXLET_WASM) {
+        store.dispatch(loading(true));
+        clearTimeout(timeout);
+        timeout = setTimeout(function () {
+            axios.post(`${PIXLET_API_BASE}/api/v1/preview`, formData)
+                .then(res => {
+                    store.dispatch(update(res.data));
+                    if ('error' in res.data) {
+                        store.dispatch(setError({ id: res.data.error, message: res.data.error }));
+                    } else {
+                        store.dispatch(clearErrors());
+                    }
+                })
+                .catch(err => {
+                    // TODO: fix this.
+                    store.dispatch(setError({ id: err, message: err }));
+                })
+                .then(() => {
+                    store.dispatch(loading(false));
+                })
+        }, 300);
+
+    } else {
+        axios.post(`${PIXLET_API_BASE}/api/v1/preview`, formData)
+            .then(res => {
+                document.title = res.data.title;
+                store.dispatch(update(res.data));
+                if ('error' in res.data) {
+                    store.dispatch(setError({ id: res.data.error, message: res.data.error }));
+                } else {
+                    store.dispatch(clearErrors());
+                }
+            })
+            .catch(err => {
+                // TODO: fix this.
+                store.dispatch(setError({ id: err, message: err }));
+            })
+            .then(() => {
+                store.dispatch(loading(false));
+            })
+
+    }
 }
