aaa9042707fb4e52abd63892fcfc202c76ce35c8
Mark Spicer <mark@tidbyt.com>
2023-05-23 10:28:57 -0400

    feat: Add controls to web view. (#774)
    This commit adds two buttons to the editor in a new section called
    "controls". One button will save the current view and the other will
    reset the editor.

---
+++ src/features/controls/Controls.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+import { Button, Stack, Grid } from '@mui/material';
+
+export default function Controls() {
+    const preview = useSelector(state => state.preview);
+
+    function downloadPreview() {
+        const date = new Date().getTime();
+        const element = document.createElement("a");
+
+        // convert base64 to raw binary data held in a string
+        let byteCharacters = atob(preview.value.webp);
+
+        // create an ArrayBuffer with a size in bytes
+        let arrayBuffer = new ArrayBuffer(byteCharacters.length);
+
+        // create a new Uint8Array view
+        let uint8Array = new Uint8Array(arrayBuffer);
+
+        // assign the values
+        for (let i = 0; i < byteCharacters.length; i++) {
+            uint8Array[i] = byteCharacters.charCodeAt(i);
+        }
+
+        const file = new Blob([uint8Array], { type: 'image/webp' });
+        element.href = URL.createObjectURL(file);
+        element.download = `tidbyt-preview-${date}.webp`;
+        document.body.appendChild(element); // Required for this to work in FireFox
+        element.click();
+    }
+
+    function resetSchema() {
+        history.replaceState(null, '', location.pathname);
+        window.location.reload();
+    }
+
+    return (
+        <Stack sx={{ marginTop: '32px' }} spacing={2} direction="row">
+            <Button variant="contained" onClick={() => downloadPreview()}>Save</Button>
+            <Button variant="contained" onClick={() => resetSchema()}>Reset</Button>
+        </Stack>
+    );
+}
