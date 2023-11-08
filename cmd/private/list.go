a3e99247dccb8f6872182d436a513f41668c314b
Mats Linander <matslina@tidbyt.com>
2023-11-08 12:30:14 -0500

    cmd: list app versions (#972)

--- cmd/private/list.go
+++ cmd/private/list.go
@@ -19,6 +19,11 @@ type TidbytApp struct {
 	OrganizationID string `json:"organizationID,omitempty"`
 }
 
+type TidbytAppVersion struct {
+	ID      string `json:"id"`
+	Created string `json:"created,omitempty"`
+}
+
 var listURL string
 
 func init() {
@@ -27,15 +32,26 @@ func init() {
 
 var ListCmd = &cobra.Command{
 	Use:   "list",
-	Short: "Lists private apps",
-	Long:  `Lists private apps, including team apps.`,
+	Short: "Lists private apps and versions",
+	Long:  `Lists private apps, or available versions of a single private app.`,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		apiToken := config.OAuthTokenFromConfig(cmd.Context())
 		if apiToken == "" {
 			return fmt.Errorf("login with `pixlet login` or use `pixlet set-auth` to configure auth")
 		}
 
-		requestURL := fmt.Sprintf("%s/v0/apps", listURL)
+		appID := ""
+		if len(args) > 0 {
+			appID = args[0]
+		}
+
+		var requestURL string
+		if appID != "" {
+			requestURL = fmt.Sprintf("%s/v0/apps/%s/versions", listURL, appID)
+		} else {
+			requestURL = fmt.Sprintf("%s/v0/apps", listURL)
+		}
+
 		req, err := http.NewRequest("GET", requestURL, nil)
 		if err != nil {
 			return fmt.Errorf("could not create http request: %w", err)
@@ -58,30 +74,65 @@ var ListCmd = &cobra.Command{
 		if resp.StatusCode != 200 {
 			return fmt.Errorf("request returned status %d with message: %s", resp.StatusCode, body)
 		}
-
 		if err != nil {
 			return fmt.Errorf("could not read response body: %w", err)
 		}
 
-		var apps struct {
-			Apps []TidbytApp `json:"apps"`
-		}
-		err = json.Unmarshal(body, &apps)
-		if err != nil {
-			return fmt.Errorf("could not parse response body: %w", err)
-		}
-
-		for _, app := range apps.Apps {
-			if !app.Private {
-				continue
-			}
-			appJson, err := json.Marshal(app)
+		if appID != "" {
+			err = listVersions(body)
 			if err != nil {
-				return fmt.Errorf("could not marshal app: %w", err)
+				return fmt.Errorf("could not list versions: %w", err)
 			}
-			fmt.Println(string(appJson))
+		} else {
+			err = listApps(body)
+			if err != nil {
+				return fmt.Errorf("could not list apps: %w", err)
+			}
 		}
 
 		return nil
 	},
+}
+
+func listApps(body []byte) error {
+	var apps struct {
+		Apps []TidbytApp `json:"apps"`
+	}
+	err := json.Unmarshal(body, &apps)
+	if err != nil {
+		return fmt.Errorf("could not parse response body: %w", err)
+	}
+
+	for _, app := range apps.Apps {
+		if !app.Private {
+			continue
+		}
+		appJson, err := json.Marshal(app)
+		if err != nil {
+			return fmt.Errorf("could not marshal app: %w", err)
+		}
+		fmt.Println(string(appJson))
+	}
+
+	return nil
+}
+
+func listVersions(body []byte) error {
+	var versions struct {
+		Versions []TidbytAppVersion `json:"versions"`
+	}
+	err := json.Unmarshal(body, &versions)
+	if err != nil {
+		return fmt.Errorf("could not parse response body: %w", err)
+	}
+
+	for _, version := range versions.Versions {
+		versionJson, err := json.Marshal(version)
+		if err != nil {
+			return fmt.Errorf("could not marshal version: %w", err)
+		}
+		fmt.Println(string(versionJson))
+	}
+
+	return nil
 }
