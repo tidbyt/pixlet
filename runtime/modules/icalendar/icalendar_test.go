c27ecd8313c17312dba5d8b7231b2ab7440696a6
Kyle C. R. Fahey <kyle.c.r.fahey@gmail.com>
2024-08-17 15:59:36 -0700

    feat: add test

--- runtime/modules/icalendar/icalendar_test.go
+++ runtime/modules/icalendar/icalendar_test.go
@@ -7,57 +7,53 @@ import (
 	"tidbyt.dev/pixlet/runtime"
 )
 
-package random_test
-
 import (
-"context"
-"testing"
-
-"github.com/stretchr/testify/assert"
-"github.com/stretchr/testify/require"
-"tidbyt.dev/pixlet/runtime"
+	"context"
 )
 
 var randomSrc = `
-load("random.star", "random")
+load("icalendar.star", "icalendar")
+raw_string = """
+	BEGIN:VCALENDAR
+	VERSION:2.0
+	CALSCALE:GREGORIAN
+	BEGIN:VTIMEZONE
+	TZID:America/Phoenix
+	LAST-MODIFIED:20231222T233358Z
+	TZURL:https://www.tzurl.org/zoneinfo-outlook/America/Phoenix
+	X-LIC-LOCATION:America/Phoenix
+	BEGIN:STANDARD
+	TZNAME:MST
+	TZOFFSETFROM:-0700
+	TZOFFSETTO:-0700
+	DTSTART:19700101T000000
+	END:STANDARD
+	END:VTIMEZONE
+	BEGIN:VEVENT
+	DTSTAMP:20240817T225510Z
+	UID:1723935287215-82939@ical.marudot.com
+	DTSTART;TZID=America/Phoenix:20240801T120000
+	RRULE:FREQ=DAILY
+	DTEND;TZID=America/Phoenix:20240801T150000
+	SUMMARY:Test
+	DESCRIPTION:Test Description
+	LOCATION:Phoenix
+	END:VEVENT
+	END:VCALENDAR
+"""
 
-min = 100
-max = 120
+def test_icalendar():
+	events = icalendar.parse(raw_string)
+	for event in events:
+		print(event)
 
-def test_number():
-	for x in range(0, 300):
-		num = random.number(min, max)
-		if num < min:
-			fail("random number less than min")
-		if num > max:
-			fail("random number greater than max")
-
-def test_seed():
-    random.seed(4711)
-    sequence = [random.number(0, 1 << 20) for _ in range(500)]
-
-    random.seed(4711) # same seed
-    for i in range(len(sequence)):
-        if sequence[i] != random.number(0, 1 << 20):
-            fail("sequence mismatch despite identical seed")
-
-    random.seed(4712) # different seed
-    different = 0
-    for i in range(len(sequence)):
-        if sequence[i] != random.number(0, 1 << 20):
-            different += 1
-    if not different:
-        fail("sequences identical despite different seeds")
-
-test_number()
-test_seed()
 
 def main():
 	return []
 `
 
-func TestRandom(t *testing.T) {
-	app, err := runtime.NewApplet("random_test.star", []byte(randomSrc))
+func TestICalendar(t *testing.T) {
+	app, err := runtime.NewApplet("icalendar_test.star", []byte(randomSrc))
 	require.NoError(t, err)
 
 	screens, err := app.Run(context.Background())
